5ee8f55f135de69d22c412a71ec499b8bc8101bf
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-10-17 23:58:46 +0530

    Update env variables

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,27 +2,31 @@ import { DatabaseType } from '../types/database';
 
 class Configs {
   get dbType(): DatabaseType {
-    return process.env.DATABASE_TYPE as DatabaseType;
+    return process.env.DBLINT_DATABASE_TYPE as DatabaseType;
   }
 
   get dbName(): string {
-    return process.env.DATABASE_NAME || '';
+    return process.env.DBLINT_DATABASE_NAME || '';
   }
 
   get dbUser(): string {
-    return process.env.DATABASE_USER || '';
+    return process.env.DBLINT_DATABASE_USER || '';
   }
 
   get dbPassword(): string {
-    return process.env.DATABASE_PASSWORD || '';
+    return process.env.DBLINT_DATABASE_PASSWORD || '';
   }
 
   get dbHost(): string {
-    return process.env.DATABASE_HOST || 'localhost';
+    return process.env.DBLINT_DATABASE_HOST || 'localhost';
   }
 
   get dbPort(): number {
-    return parseInt(process.env.DATABASE_PORT || '5432');
+    return parseInt(process.env.DBLINT_DATABASE_PORT || '5432');
+  }
+
+  get ignoreTables(): string[] {
+    return JSON.parse(process.env.DBLINT_IGNORE_TABLES || '[]');
   }
 }
 
