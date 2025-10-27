a6b9b6052810e087532d38dca6eff41336464338
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-10-27 21:16:10 +0530

    Refactoring work

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
+import fs from 'fs';
+import linterFactory from './core/lint/linter-factory';
+import CLIReporter from './core/reporter/cli-reporter';
 
-console.log('Hello, World!');
-console.log('dblint is coming soon...');
-console.log('check out the repo: https://github.com/yusufshakeel/dblint');
+async function main() {
+  const linter = linterFactory();
+  const report = await linter.lint();
+
+  fs.writeFileSync('./report/dblint-report.json', JSON.stringify(report, null, 2));
+
+  CLIReporter.report(report);
+
+  if (report.stats.validations.error !== report.stats.validations.ignoredError) {
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
