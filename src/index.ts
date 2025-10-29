5feb05f1ed1cf82e90343bd3d11eba3932b6de57
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-10-29 12:58:16 +0530

    Update, refactoring work

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,8 @@
 #!/usr/bin/env node
-import fs from 'fs';
 import linterFactory from './core/lint/linter-factory';
-import CLIReporter from './core/reporter/cli-reporter';
+import { Lint } from './types/lint';
 
-async function main() {
-  const reportDir = process.cwd() + '/report';
-  const outputFileName = 'dblint-report.json';
-
-  if (!fs.existsSync(reportDir)) {
-    console.log('Report directory does not exist. Creating...');
-    console.log('Directory path:', reportDir);
-    fs.mkdirSync(reportDir, { recursive: true });
-    console.log('Directory created successfully.');
-  }
-
+export async function dblint(): Promise<Lint> {
   const linter = linterFactory();
-  const report = await linter.lint();
-
-  const outputFilePath = `${reportDir}/${outputFileName}`;
-
-  fs.writeFileSync(outputFilePath, JSON.stringify(report, null, 2));
-
-  CLIReporter.report(report);
-
-  console.log(`\n📁 Report generated at: ${outputFilePath}\n`);
-
-  if (report.stats.validations.error !== report.stats.validations.ignoredError) {
-    process.exit(1);
-  } else {
-    process.exit(0);
-  }
+  return await linter.lint();
 }
-
-main().catch(error => {
-  console.error(error);
-  process.exit(1);
-});
